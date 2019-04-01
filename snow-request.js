954678162317c6f6ef90f2999b7350bae32f3c46
Yan Hong <yan@hackhub.com>
2019-03-31 21:26:18 -0700

    fix issue date html element index

--- snow-request.js
+++ snow-request.js
@@ -231,7 +231,7 @@ var SnowRequest = function() {
       //Find out if response is in metric or not.
       forecastOptObj.isMetric = $('.deg-c input').attr('checked') === 'checked';
       //Extrapolate time-relevant information needed to build forecast, and build object.
-      var issued = TimeUtil.fixIssueDateFormat($($('.location-issued__no-wrap')[6]).text() + $($('.location-issued__no-wrap')[7]).text());
+      var issued = TimeUtil.fixIssueDateFormat($($('.location-issued__no-wrap')[5]).text() + $($('.location-issued__no-wrap')[6]).text());
       forecastOptObj.issuedDate = issued;
       forecastOptObj.startDay = $($('table tr.day-names td')[0]).text();
       var match = issued.match(/^\d+/);
